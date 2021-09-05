8223a0e52c3220de970db5eb2e5712f851aa4770
shyboy13 <shyboy130301@hotmail.com>
2021-09-05 12:18:21 -0700

    Improving buttons and a little mobile support

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,20 +1,24 @@
+
+
 class PomodoroTimer {
   constructor(mins, element, stopButton, finishButton, elemPomodoroNum, resetButton, typeTimerCallback){
+    this.clickSound = new Audio('click-sound.wav');
     // typeTime represents if the timer is a pomodoro or is a break
-    this.typeTimer = 'pomodoro';
+    this.typeTimer = 'Pomodoro';
     // function that will be called when typeTimer change
     this.typeTimerCallback = typeTimerCallback;
     // stateTimer represents if timer is paused or not 
     this.stateTimer = false;
     this.elemPomodoroNum = elemPomodoroNum;
+    this.elemPomodoroNum.textContent = 0;
 
     this.shortBreakTime = new Date(0, 0, 0, 0, 5);
     this.longBreakTime = new Date(0, 0, 0, 0, 20); 
     
     // for remember the minutes of the pomodoro
-    this.initialTime = mins;
+    this.pomodoroTime = mins;
 
-    this.pomodoroTime = new Date(0, 0, 0, 0, mins-1, 59);
+    this.time = new Date(0, 0, 0, 0, mins-1, 59);
     this.countingTimer = null;
 
     this.element =  element;
@@ -26,67 +30,79 @@ class PomodoroTimer {
     this.finishButton.style.display = "none";
 
     // save the current number of pomodoros and verify if there are pomodoros 
-    if(sessionStorage.getItem('pomodoroNumber') == null){
-      sessionStorage.setItem('pomodoroNumber', 1);
+    if(localStorage.getItem('pomodoroNumber') == null){
+      localStorage.setItem('pomodoroNumber', 0);
+      this.elemPomodoroNum.textContent = Number(localStorage.getItem('pomodoroNumber'));
     }else {
-      this.elemPomodoroNum.textContent = Number(sessionStorage.getItem('pomodoroNumber')) - 1;
+      this.elemPomodoroNum.textContent = Number(localStorage.getItem('pomodoroNumber'));
       this.elemPomodoroNum.style.display = "block";
-      console.log(typeof sessionStorage.getItem('pomodoroNumber'));
+      console.log(typeof localStorage.getItem('pomodoroNumber'));
     }
 
     this.resetButton.addEventListener('click', () => {
+      this.clickSound.pause();
+      this.clickSound.currentTime = 0;
+      this.clickSound.play();
       this.resetTimer();
-      this.element.textContent = this.pomodoroTime.getMinutes();
+      this.element.textContent = this.time.getMinutes();
     });
 
     this.stopButton.addEventListener('click', () => {
       if (!this.stateTimer) {
+        this.clickSound.pause();
+        this.clickSound.currentTime = 0;
+        this.clickSound.play();
         this.resumeTimer();
-        this.stopButton.textContent = 'PAUSAR';
-        this.finishButton.style.display = "none";
       } else {
-        this.pauseTimer('REANUDAR', true); 
+        this.clickSound.pause();
+        this.clickSound.currentTime = 0;
+        this.clickSound.play();
+        this.pauseTimer('REANUDAR', false); 
         this.finishButton.style.display = "block";
       }
     });  
 
     this.finishButton.addEventListener('click', () =>{
-      this.finishTimer();
+      this.clickSound.pause();
+      this.clickSound.currentTime = 0;
+      this.clickSound.play();
+      this.skipTimer();
     });
   } // constructor ends
 
   // verify if the timer has finished and save the remaning time 
   startTimer(){
-    if ((this.pomodoroTime.getMinutes() === 0) && (this.pomodoroTime.getSeconds() == 0)){
-      this.pauseTimer('INICIAR', false);
-      this.checkTypeTimer();     
+    if ((this.time.getMinutes() === 0) && (this.time.getSeconds() == 0)){
+      this.pauseTimer('INICIAR', true);
+      this.setTimer();     
     } else {
-      this.pomodoroTime = new Date(this.pomodoroTime - new Date(1000));
-      this.element.textContent = this.pomodoroTime.getMinutes() + ':' + this.pomodoroTime.getSeconds(); 
+      this.time = new Date(this.time - new Date(1000));
+      this.element.textContent = this.time.getMinutes() + ':' + this.time.getSeconds(); 
     }
   }
-  // this function check if the previous timer is a pomodoro or a break 
-  checkTypeTimer(){
-    // if it is a break change the typeTimer to pomodoro and viceversa 
-    if (this.typeTimer === 'shortBreak' || this.typeTimer === 'longBreak'){
-      this.typeTimer = 'pomodoro';
+  // this function set a timer as its argument if it's defined else check if the previous timer is a pomodoro
+  // or a break and if it is a break change the typeTimer to pomodoro and viceversa 
+  setTimer(explicit){
+    if ((this.typeTimer === 'short break' || this.typeTimer === 'long break') || explicit == 'Pomodoro'){
+      this.typeTimer = 'Pomodoro';
       this.resetTimer(); 
-      this.element.textContent = this.pomodoroTime.getMinutes();
+      this.element.textContent = this.time.getMinutes();
       this.typeTimerCallback('pomodoro');
-    } else if ((this.typeTimer === 'pomodoro') && (Number(sessionStorage.getItem('pomodoroNumber')) % 4 === 0)) {
-      this.setBreak('longBreak');
-    } else { 
-      this.setBreak('shortBreak');
+    } else if (((this.typeTimer === 'Pomodoro') && (Number(localStorage.getItem('pomodoroNumber')) % 3 === 0) && (Number(localStorage.getItem('pomodoroNumber')) !== 0)) || explicit == 'long break') {
+      this.setBreak('long break');
+    } else if (explicit == undefined || explicit == 'short break'){ 
+      this.setBreak('short break');
     }
   }
   // pauses the timer and change the text of the button
-  pauseTimer(textStopBtn, showSeconds){
+  pauseTimer(textStopBtn, isStart){
     clearInterval(this.countingTimer);
     this.stopButton.textContent = textStopBtn;
-    if (showSeconds && this.pomodoroTime.getSeconds() != 0){
-      this.element.textContent = this.pomodoroTime.getMinutes()+':'+this.pomodoroTime.getSeconds();
+    if (isStart){
+      this.element.textContent = this.time.getMinutes();
+      this.finishButton.style.display = "none";
     } else {
-      this.element.textContent = this.pomodoroTime.getMinutes();
+      this.element.textContent = this.time.getMinutes()+':'+this.time.getSeconds();
     }
     this.stateTimer = false;
   }
@@ -94,42 +110,49 @@ class PomodoroTimer {
   resumeTimer(){
     this.countingTimer = setInterval(() => {this.startTimer()}, 1000);
     this.stateTimer = true;
+    this.stopButton.textContent = 'PAUSAR';
+    this.finishButton.style.display = "none";
   }
   resetTimer(){
-    if (this.typeTimer === 'pomodoro'){
-      this.pomodoroTime = new Date(0, 0, 0, 0, this.initialTime);
-    } else if (this.typeTimer == 'shortBreak'){
-      this.pomodoroTime = this.shortBreakTime;
-    } else if (this.typeTimer == 'longBreak'){
-      this.pomodoroTime = this.longBreakTime;
+    if (this.typeTimer === 'Pomodoro'){
+      this.time = new Date(0, 0, 0, 0, this.pomodoroTime);
+    } else if (this.typeTimer == 'short break'){
+      this.time = this.shortBreakTime;
+    } else if (this.typeTimer == 'long break'){
+      this.time = this.longBreakTime;
     }
   }
-
-  finishTimer(){
+  
+  skipTimer(){
     if (confirm("Desea pasar de este timer?")) {
-      this.pomodoroTime = new Date(0, 0, 0, 0, 0, 0);
+      this.time = new Date(0, 0, 0, 0, 0, 0);
       this.startTimer();
     }
   }
   // change the timer to a break and add a pomodoro
   setBreak(breakType){
-    if (breakType === 'shortBreak'){
-      this.pomodoroTime = this.shortBreakTime;
-      this.typeTimer = 'shortBreak';
-      this.typeTimerCallback('shortBreak');
+    if (breakType === 'short break'){
+      this.time = this.shortBreakTime;
+      this.typeTimer = 'short break';
+      this.typeTimerCallback('short break');
     }
-    if (breakType === 'longBreak'){
-      this.pomodoroTime = this.longBreakTime;
-      this.typeTimer = 'longBreak';
-      this.typeTimerCallback('longBreak');
+    if (breakType === 'long break'){
+      this.time = this.longBreakTime;
+      this.typeTimer = 'long break';
+      this.typeTimerCallback('long break');
     }
     this.addPomodoro();
   }
   addPomodoro(){
-    this.element.textContent = this.pomodoroTime.getMinutes();
-    sessionStorage.setItem('pomodoroNumber', Number(sessionStorage.getItem('pomodoroNumber')) + 1);
-    this.elemPomodoroNum.textContent = Number(sessionStorage.getItem('pomodoroNumber')) - 1;
+    this.element.textContent = this.time.getMinutes();
+    localStorage.setItem('pomodoroNumber', Number(localStorage.getItem('pomodoroNumber')) + 1);
+    this.elemPomodoroNum.textContent = Number(localStorage.getItem('pomodoroNumber'));
     this.elemPomodoroNum.style.display = "block";
+  }
+  resetPomodoros(){
+    localStorage.setItem('pomodoroNumber', 0);
+    this.elemPomodoroNum.textContent = Number(localStorage.getItem('pomodoroNumber'));
+    this.setTimer('Pomodoro');
   }
 }
 
@@ -139,20 +162,31 @@ const timerElem = document.querySelector('#timer');
 const pomodoroNumber = document.querySelector('#pomodoro-number');
 const resetButton = document.querySelector('#reset-button');
 const actualTimer = document.querySelector('#actual-timer');
+const optionsButtons = document.querySelector('#options');
 
 const timer = new PomodoroTimer(25, timerElem, stopButton, finishButton, pomodoroNumber, resetButton, (typeTimer) => {
   actualTimer.textContent = timer.typeTimer;
   if (typeTimer === 'pomodoro'){
     document.querySelector('#timer').style.backgroundColor = '#9e3f3f';
-  } else if (typeTimer == 'shortBreak'){
+    document.querySelector('#timer').style.borderColor = '#8e2f2f';
+  } else if (typeTimer == 'short break'){
     document.querySelector('#timer').style.backgroundColor = '#25cc8c';
     document.querySelector('#timer').style.borderColor = '#15bc7c';
-  } else if (typeTimer == 'longBreak'){
-    document.querySelector('#timer').style.backgroundColor = '#98f6a8';
+  } else if (typeTimer == 'long break'){
+    document.querySelector('#timer').style.backgroundColor = '#25cc8c';
+    document.querySelector('#timer').style.borderColor = '#15bc7c';
   }
 });
 
+const resetPomodorosBtn = document.querySelector('#reset-pomodoros-btn');
+console.log(resetPomodorosBtn);
+
+resetPomodorosBtn.onclick = () => {
+  timer.resetPomodoros();
+};
 actualTimer.textContent = timer.typeTimer;
+
+
 
 
 
